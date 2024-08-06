90bec264771e1b6867db3f9fa554af12d2d56a4a
Fatoumatab8274 <fatoumatab8274@nycstudents.net>
2024-08-06 16:04:46 +0000

    built age verification

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
-console.log('hello world!')
+// console.log('hello world!')
 
-var prompt = prompt( 'what is you name?')
+// var prompt = prompt( 'what is your name?')
 
-console.log(prompt)
+// console.log(prompt) 
+
+
+function verifyAge (age){
+var age = prompt('what is your age?');
+console.log(age)
+
+  
+  if (age < 18){
+    alert('you are a minor')
+  } else if (age >= 18 && age <= 60) {
+    alert( 'You are an adult!')
+  } else {
+    alert('You are a senior citizen')
+  }
+  
+}        
