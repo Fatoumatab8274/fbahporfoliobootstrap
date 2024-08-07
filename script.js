4ee01017d1efd938c848f839b47f486e7d7d2d83
Fatoumatab8274 <fatoumatab8274@nycstudents.net>
2024-08-07 16:50:54 +0000

    upload

--- script.js
+++ script.js
@@ -17,5 +17,22 @@ console.log(age)
   } else {
     alert('You are a senior citizen')
   }
+}
+
+  function uploadAndIdentifyPlant() {
+    // get the photo from the frontend
+    var photoInput = document.getElementById('photoInput');
+    console.log(photoInput.files[0])
+
+    // if no photo was selected and the user clicks submit 
+    // let the user know they need to upload a photo first
+    if (photoInput.files.length === 0) {
+      alert('Please select a photo to upload');
+      return;
+    }
+
+    //select first file from the filrs folder/arrayfrom the pload button 
+    var selectedFile = photoInput.files[0];
+  }
+
   
-}        
